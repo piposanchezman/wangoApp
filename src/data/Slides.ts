ccbbbb6a909697b5457bd4a9db022633f51b7a22
Andres Felipe Sanchez Manjarres <piposanchezman@gmail.com>
2024-06-17 21:14:13 -0500

    feat: Add Slides data for carousel

---
+++ src/data/Slides.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    image: require("../assets/Carousel1.webp"),
+    title: "Administra tus lotes",
+    description: "Crea, edita y elimina tus cultivos",
+  },
+  {
+    id: 2,
+    image: require("../assets/Carousel2.webp"),
+    title: "Calcula la huella hídrica",
+    description: "Realiza registros y obtén analíticas",
+  },
+];
